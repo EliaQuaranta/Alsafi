5d4f8eb087460b886e995dcd5ae537ec798619d8
EliaQuaranta <eliaquaranta40@gmail.com>
2024-01-08 19:40:09 +0100

    header test

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 function Header() {
   return(
 
-    <div className="hero bg-base-200">
+    <div className="hero pt-20 bg-[#4366C9] ">
     <div className="hero-content flex-col lg:flex-row">
       <div className='text-center'>
-        <h1 className="text-5xl font-bold text-center">Titolo</h1>
-        <p className="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure consectetur, maiores error earum quo minima doloribus atque explicabo, blanditiis rem quas corrupti, accusamus aliquam excepturi veniam nesciunt. Facere, dignissimos officia!</p>
+        <h1 className="text-5xl font-bold text-center font-link ">Titolo</h1>
+        <p className="py-6 pt-20 font-link text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure consectetur, maiores error earum quo minima doloribus atque explicabo, blanditiis rem quas corrupti, accusamus aliquam excepturi veniam nesciunt. Facere, dignissimos officia!</p>
         <button className="btn btn-primary">Get Started</button>
       </div>
     </div>
